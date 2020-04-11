a1f7b4c7fd5f6bdc0002e97559fe16cea2feb7b9
ayoubleb <lebhal@gmail.com>
2020-04-11 03:09:28 +0100

    Initial commit

---
+++ src/test/java/com/sti/quranapi/QuranapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sti.quranapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuranapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
